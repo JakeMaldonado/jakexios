21908c159c9108d8b1f43c2b45d2b26ec55558bc
JakeMaldonado <jakemaldonado@protonmail.com>
2018-11-07 14:59:54 -0500

    added GET, POST, and DELETE to jakexios

---
+++ index.js
@@ -0,0 +1,53 @@
+export default class Jakexios {
+    async get(url, headers) {
+      try {
+        if(!headers) {
+          let res = await fetch(url);
+          return await res.json();
+        }
+        let init = {
+          method: 'GET',
+          headers: headers
+        }
+        let res = await fetch(url, init);
+        return await res.json();
+      } catch (e) {
+        console.log(`error getting: ${url}`);
+        console.log(e);
+        return [];
+      }
+    }
+  
+    async post(url, body, headers) {
+      try {
+        headers && (headers = { "Content-Type": "application/json; charset=utf-8" });
+        let init = {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify(body)
+        };
+        let res = await fetch(url, init);
+        return await res.json();
+      } catch (e) {
+        console.log(`error posting: ${url}`);
+        console.log(e);
+        return [];
+      }
+    }
+  
+    async delete(url, headers) {
+      try {
+        headers && (headers = { "Content-Type": "application/json; charset=utf-8" });
+        let init = {
+          method: 'DELETE',
+          headers: headers
+        };
+        let res = await fetch(url, init);
+        return await res.json();
+      } catch (e) {
+        console.log(`error deleting: ${url}`);
+        console.log(e);
+        return [];
+      }
+    }
+  }
